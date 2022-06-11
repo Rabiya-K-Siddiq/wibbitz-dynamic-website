6bd0824cd376cdfd10977fcbccf52d9c5651b7c4
Rabiya K <rabyabinthsiddiqk23@gmail.com>
2022-06-11 09:42:29 +0530

    added | contact model

--- web/admin.py
+++ web/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from web.models import Subscription, Clients, Feature
+from web.models import Subscription, Clients, Feature, Contact
 
 admin.site.register(Subscription)
 
@@ -9,3 +9,9 @@ class FeatureAdmin(admin.ModelAdmin):
     list_display = ["id", "title", "description"]
 
 admin.site.register(Feature)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ["id", "first_name", "company", "jobe_role"]
+
+admin.site.register(Contact)
--- web/models.py
+++ web/models.py
@@ -1,16 +1,49 @@
 from django.db import models
 
+
+COMPANY_SIZE = (
+    ('1', '1-10'),
+    ('2', '11-50'),
+    ('3', '51-200'),
+    ('4', '201-500'),
+)
+
+
+INDUSTRY_TYPE = (
+    ('agriculture', 'Agriculture'),
+    ('banking & finance', 'Banking & Finance'),
+    ('business services & software', 'Business services & Software'),
+)
+
+
+JOB_ROLE = (
+    ('c-suite', 'C-Suite'),
+    ('vp', 'VP'),
+)
+
+
+COUNTRY_CHOICE = (
+    ('united states', 'united states'),
+    ('india', 'india'),
+    ('afganistan', 'afganistan'),
+    ('america', 'america'),
+    ('albania', 'albania'),
+)
+
+
 class Subscription(models.Model):
     email = models.EmailField()
 
     def __str__(self):
         return self.email
 
+
 class Clients(models.Model):
     logo = models.FileField(upload_to='clients/')
 
     def __str__(self):
         return self.logo.url
+
 
 class Feature(models.Model):
     image = models.ImageField(upload_to='features/')
@@ -26,4 +59,22 @@ class Feature(models.Model):
     def __str__(self):
         return self.title
     
+
+class Contact(models.Model):
+    email = models.EmailField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+    company_size = models.CharField(max_length=255, choices=COMPANY_SIZE)
+    industry = models.CharField(max_length=128, choices=INDUSTRY_TYPE)
+    job_role = models.CharField(max_length=128, choices=JOB_ROLE)
+    country = models.CharField(max_length=128, choices=COUNTRY_CHOICE)
+    privacy_policy = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "web_contact"
+        ordering = ["-id"]
+
+    def __str__(self):
+        return self.first_name
 
